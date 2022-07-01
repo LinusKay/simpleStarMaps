01859c9d9b0977a964adaaf4f8e65f8c79a7239b
Linus Kay s3668218 <s3668218@student.rmit.edu.au>
2022-07-01 17:09:35 +1000

    Add depth blur, export images to ./out

--- generate_starmaps.py
+++ generate_starmaps.py
@@ -10,7 +10,7 @@ starColourMinMax = (150, 255)
 starSizeMinMax = (1, 5)
 primaryStarSizeMinMax = (8, 12)
 starCountMinMax = (6, 130)
-primaryStarCountMinMax = (4, 8)
+primaryStarCountMinMax = (3, 8)
 primaryStarBorder = 50
 
 mapCount = 0
@@ -21,12 +21,13 @@ def primaryStarBoundsLimit(num):
 
 for starmap in range(STARMAPCOUNT):
     im = Image.new('RGB', (imgSize, imgSize), (backColour, backColour, backColour))
-    draw = ImageDraw.Draw(im)
+    
 
     maxStars = random.randint(starCountMinMax[0], starCountMinMax[1])
     primaryStarCount = random.randint(primaryStarCountMinMax[0], primaryStarCountMinMax[1])
 
     for star in range(maxStars):
+        draw = ImageDraw.Draw(im)
         if starCount < primaryStarCount:
             starSize = random.randint(primaryStarSizeMinMax[0], primaryStarSizeMinMax[1])
             x0 = primaryStarBoundsLimit(random.randint(0,imgSize))
@@ -43,8 +44,8 @@ for starmap in range(STARMAPCOUNT):
         starColour = random.randint(starColourMinMax[0], starColourMinMax[1])
         draw.ellipse((x0, y0, x1, y1), fill=(starColour, starColour, starColour), outline=(0, 0, 0))
         starCount += 1
-
-    im = im.filter(ImageFilter.GaussianBlur(3))
-    im.save(str(mapCount) + '.jpg', quality=95)
+        im = im.filter(ImageFilter.GaussianBlur(.5))
+    
+    im.save('./out/' + str(mapCount) + '.jpg', quality=95)
     mapCount += 1
     starCount = 0
